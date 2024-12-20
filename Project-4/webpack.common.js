fe3713354ace70ae10097a75649e769151c6bb98
Rahma Nashaat <rahmanashaat973@gmail.com>
2024-12-20 11:37:04 +0200

    Add files via upload

---
+++ Project-4/webpack.common.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+};
---
+++ Project-4/webpack.dev.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'source-map',
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        port: 3000,
+        open: true,
+        hot: true,
+    },
+});
